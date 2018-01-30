02046feb456e1786b4350443c3f6c31933a673a1
MaitreMenard <maxime.menard.1@ulaval.ca>
2018-01-30 16:46:20 -0500

    Fixed code style

--- lab2/src/ofApp.cpp
+++ lab2/src/ofApp.cpp
@@ -1,71 +1,76 @@
 #include "ofApp.h"
 
-//--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup()
+{
 
 }
 
-//--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update()
+{
 
 }
 
-//--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw()
+{
 
 }
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::exit()
+{
 
 }
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::keyPressed(int key)
+{
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::keyReleased(int key)
+{
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseMoved(int x, int y)
+{
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button)
+{
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button)
+{
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::mouseReleased(int x, int y, int button)
+{
 
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseEntered(int x, int y)
+{
 
 }
 
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::mouseExited(int x, int y)
+{
 
 }
 
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::windowResized(int w, int h)
+{
 
 }
 
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::gotMessage(ofMessage msg)
+{
+
+}
+
+void ofApp::dragEvent(ofDragInfo dragInfo)
+{
 
 }
